53a14526ae2ff4658343b4839de38a7b5932bcbc
Matteo <matteo.tiozzo.1@studenti.unipd.it>
2024-03-26 16:14:14 +0100

    change banner writing

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -12,7 +12,7 @@ export const Banner = () => {
   const [delta, setDelta] = useState(300 - Math.random() * 100);
   const [index, setIndex] = useState(1);
   const toRotate = [
-    "Computer Science Unipd student!",
+    "Computing Unipd student!",
     "cyber newbie!",
     "travel lover!",
   ];
