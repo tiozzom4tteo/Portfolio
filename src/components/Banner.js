c5b67fc8a038aafeedea3d99a067ca4abaac71c9
Matteo <matteo.tiozzo.1@studenti.unipd.it>
2024-03-28 22:12:49 +0100

    fix deploy

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -63,7 +63,6 @@ export const Banner = () => {
 
   const responsive = {
     superLargeDesktop: {
-      // the naming can be any, depends on you.
       breakpoint: { max: 4000, min: 3000 },
       items: 5,
     },
